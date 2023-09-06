bfb5f5721d26155991bb50ac609a61ed0009201f
Alvaro <alvarodc99@hotmail.com>
2023-09-06 15:19:01 -0300

    creo que funciona

--- js/space.js
+++ js/space.js
@@ -1,52 +1,47 @@
 document.addEventListener('DOMContentLoaded', function () {
     const searchInput = document.getElementById('inputBuscar');
-    const searchButton = document.getElementById('btnBuscar');
-    const resultsContainer = document.getElementById('contenedor');
+    const searchBtn = document.getElementById('btnBuscar');
+    const container = document.getElementById('contenedor');
 
-    searchButton.addEventListener('click', () => {
-        const searchText = searchInput.value.trim();
-
-        if (searchText === '') {
-            alert('Por favor, ingrese un término de búsqueda.');
-            return;
+    searchBtn.addEventListener('click', function () {
+        const search = searchInput.value;
+        if (search) {
+            const API = `https://images-api.nasa.gov/search?q=${search}`;
+            fetch(API)
+                .then((response) => response.json())
+                .then((data) => {
+                    showData(data);
+                })
+                .catch((error) => {
+                    console.error('ERROR', error);
+                });
         }
-
-        const apiUrl = `https://images-api.nasa.gov/search?q=` + searchText;
-
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                resultsContainer.innerHTML = '';
-
-                if (data.collection && data.collection.items.length > 0) {
-                    data.collection.items.forEach(item => {
-                        const imageUrl = item.links[0].href;
-                        const title = item.data[0].title;
-                        const description = item.data[0].description || 'No disponible';
-                        const dateCreated = item.data[0].date_created || 'No disponible';
-
-                        const resultItem = document.createElement('div');
-                        resultItem.classList.add('col-md-4', 'result-item'); // Add col-md-4 class
-                        resultItem.innerHTML = `
-                            <div class="card mb-4">
-                                <img src="${imageUrl}" class="card-img-top" alt="${title}">
-                                <div class="card-body">
-                                    <h2 class="card-title">${title}</h2>
-                                    <p class="card-text"><strong>Descripción:</strong> ${description}</p>
-                                    <p class="card-text"><strong>Fecha:</strong> ${dateCreated}</p>
-                                </div>
-                            </div>
-                        `;
-
-                        resultsContainer.appendChild(resultItem);
-                    });
-                } else {
-                    resultsContainer.innerHTML = '<p>No se encontraron resultados.</p>';
-                }
-            })
-            .catch(error => {
-                console.error('Error al realizar la solicitud:', error);
-                resultsContainer.innerHTML = '<p>Error al buscar imágenes.</p>';
-            });
     });
+
+    function showData(data) {
+        container.innerHTML = '';
+        const items = data.collection.items;
+        for (let i = 0; i < items.length; i++) {
+            const item = items[i];
+            const image = item.links[0].href;
+            const title = item.data[0].title;
+            const description = item.data[0].description;
+            const date = item.data[0].date_created;
+
+            const card = document.createElement('div');
+            card.classList.add('col-lg-4', 'col-md-6', 'mb-3'); // Bootstrap grid classes
+            card.innerHTML = `
+                <div class="card">
+                    <img src="${image}" class="card-img-top" style="height: 500px" alt="${title}">
+                    <div class="card-body">
+                        <h5 class="card-title">${title}</h5>
+                        <div class="card-text" style="max-height: 100px; overflow: auto;">${description}</div>
+                        <p class="card-text"><small class="text-muted">${date}</small></p>
+                    </div>
+                </div>
+            `;
+
+            container.appendChild(card);
+        }
+    }
 });
