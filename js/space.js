bcee0aafac859a9b058b6696596ccde0cc70600e
Alvaro <alvarodc99@hotmail.com>
2023-09-06 14:45:13 -0300

    agregue bootstrap

--- js/space.js
+++ js/space.js
@@ -1,49 +1,52 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const searchInput = document.getElementById('inputBuscar');
-  const searchButton = document.getElementById('btnBuscar');
-  const resultsContainer = document.getElementById('contenedor');
+    const searchInput = document.getElementById('inputBuscar');
+    const searchButton = document.getElementById('btnBuscar');
+    const resultsContainer = document.getElementById('contenedor');
 
-  searchButton.addEventListener('click', () => {
-      const searchText = searchInput.value.trim();
+    searchButton.addEventListener('click', () => {
+        const searchText = searchInput.value.trim();
 
-      if (searchText === '') {
-          alert('Por favor, ingrese un término de búsqueda.');
-          return;
-      }
+        if (searchText === '') {
+            alert('Por favor, ingrese un término de búsqueda.');
+            return;
+        }
 
-      const apiUrl = `https://images-api.nasa.gov/search?q=` + searchText;
+        const apiUrl = `https://images-api.nasa.gov/search?q=` + searchText;
 
-      fetch(apiUrl)
-          .then(response => response.json())
-          .then(data => {
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                resultsContainer.innerHTML = '';
 
-            resultsContainer.innerHTML = '';
+                if (data.collection && data.collection.items.length > 0) {
+                    data.collection.items.forEach(item => {
+                        const imageUrl = item.links[0].href;
+                        const title = item.data[0].title;
+                        const description = item.data[0].description || 'No disponible';
+                        const dateCreated = item.data[0].date_created || 'No disponible';
 
-              if (data.collection && data.collection.items.length > 0) {
-                  data.collection.items.forEach(item => {
-                      const imageUrl = item.links[0].href;
-                      const title = item.data[0].title;
-                      const description = item.data[0].description || 'No disponible';
-                      const dateCreated = item.data[0].date_created || 'No disponible';
+                        const resultItem = document.createElement('div');
+                        resultItem.classList.add('col-md-4', 'result-item'); // Add col-md-4 class
+                        resultItem.innerHTML = `
+                            <div class="card mb-4">
+                                <img src="${imageUrl}" class="card-img-top" alt="${title}">
+                                <div class="card-body">
+                                    <h2 class="card-title">${title}</h2>
+                                    <p class="card-text"><strong>Descripción:</strong> ${description}</p>
+                                    <p class="card-text"><strong>Fecha:</strong> ${dateCreated}</p>
+                                </div>
+                            </div>
+                        `;
 
-                      const resultItem = document.createElement('div');
-                      resultItem.classList.add('result-item');
-                      resultItem.innerHTML = `
-                          <img src="${imageUrl}" alt="${title}">
-                          <h2>${title}</h2>
-                          <p><strong>Descripción:</strong> ${description}</p>
-                          <p><strong>Fecha:</strong> ${dateCreated}</p>
-                      `;
-
-                      resultsContainer.appendChild(resultItem);
-                  });
-              } else {
-                  resultsContainer.innerHTML = '<p>No se encontraron resultados.</p>';
-              }
-          })
-          .catch(error => {
-              console.error('Error al realizar la solicitud:', error);
-              resultsContainer.innerHTML = '<p>Error al buscar imágenes.</p>';
-          });
-  });
+                        resultsContainer.appendChild(resultItem);
+                    });
+                } else {
+                    resultsContainer.innerHTML = '<p>No se encontraron resultados.</p>';
+                }
+            })
+            .catch(error => {
+                console.error('Error al realizar la solicitud:', error);
+                resultsContainer.innerHTML = '<p>Error al buscar imágenes.</p>';
+            });
+    });
 });
