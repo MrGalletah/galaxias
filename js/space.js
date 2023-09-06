58b2b93cb18d66b904921dad7481177d4678cec6
Rodrigo Martinez <ropecos94@gmail.com>
2023-09-06 15:15:33 -0300

    Update space.js

--- js/space.js
+++ js/space.js
@@ -21,25 +21,56 @@ document.addEventListener('DOMContentLoaded', function () {
     function showData(data) {
         container.innerHTML = '';
         const items = data.collection.items;
+    
+        // Crear un div que actuará como fila (row) para las cartas
+        let row = document.createElement('div');
+        row.classList.add('row');
+    
         for (let i = 0; i < items.length; i++) {
             const item = items[i];
             const image = item.links[0].href;
             const title = item.data[0].title;
             const description = item.data[0].description;
             const date = item.data[0].date_created;
-
+    
+            // Crear una columna para cada carta
+            const col = document.createElement('div');
+            col.classList.add('col-md-4', 'mb-3'); // 'mb-3' para agregar margen inferior a las columnas
+    
             const card = document.createElement('div');
-            card.classList.add('card', 'mb-3');
-            card.innerHTML = `
-              <img src="${image}" class="card-img-top" alt="${title}">
-              <div class="card-body">
+            card.classList.add('card');
+    
+            const cardImage = document.createElement('img');
+            cardImage.classList.add('card-img-top');
+            cardImage.src = image; // Establecer la imagen
+            cardImage.alt = title; // Establecer el texto alternativo
+            cardImage.style.height = '200px'; // Establecer una altura fija para la imagen
+    
+            const cardBody = document.createElement('div');
+            cardBody.classList.add('card-body');
+            cardBody.innerHTML = `
                 <h5 class="card-title">${title}</h5>
                 <p class="card-text">${description}</p>
                 <p class="card-text"><small class="text-muted">${date}</small></p>
-              </div>
             `;
-
-            container.appendChild(card);
+    
+            card.appendChild(cardImage);
+            card.appendChild(cardBody);
+    
+            col.appendChild(card);
+            row.appendChild(col);
+    
+            // Después de agregar tres cartas, crea una nueva fila
+            if ((i + 1) % 3 === 0) {
+                container.appendChild(row);
+                row = document.createElement('div');
+                row.classList.add('row');
+            }
+        }
+    
+        // Asegúrate de agregar la última fila si no hay un número exacto de cartas
+        if (items.length % 3 !== 0) {
+            container.appendChild(row);
         }
     }
 });
