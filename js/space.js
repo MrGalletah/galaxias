f3037b736a2d9cb2aaf24bcb1f7bc7e47bec6fff
Rodrigo Martinez <ropecos94@gmail.com>
2023-09-06 14:35:33 -0300

    Update space.js

--- js/space.js
+++ js/space.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
             fetch(API)
                 .then((response) => response.json())
                 .then((data) => {
-                    mostrarDatos(data);
+                    showData(data);
                 })
                 .catch((error) => {
                     console.error('ERROR', error);
@@ -18,24 +18,24 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    function mostrarDatos(data) {
+    function showData(data) {
         container.innerHTML = '';
         const items = data.collection.items;
         for (let i = 0; i < items.length; i++) {
             const item = items[i];
-            const imagen = item.links[0].href;
-            const titulo = item.data[0].title;
-            const descripcion = item.data[0].description;
-            const fecha = item.data[0].date_created;
+            const image = item.links[0].href;
+            const title = item.data[0].title;
+            const description = item.data[0].description;
+            const date = item.data[0].date_created;
 
             const card = document.createElement('div');
             card.classList.add('card', 'mb-3');
             card.innerHTML = `
-              <img src="${imagen}" class="card-img-top" alt="${titulo}">
+              <img src="${image}" class="card-img-top" alt="${title}">
               <div class="card-body">
-                <h5 class="card-title">${titulo}</h5>
-                <p class="card-text">${descripcion}</p>
-                <p class="card-text"><small class="text-muted">${fecha}</small></p>
+                <h5 class="card-title">${title}</h5>
+                <p class="card-text">${description}</p>
+                <p class="card-text"><small class="text-muted">${date}</small></p>
               </div>
             `;
 
